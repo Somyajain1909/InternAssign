ba2710a0211f60ab971697a2c997ced2f581900d
Somyajain1909 <202052339@iiitv.ac.in>
2023-05-26 17:21:26 +0530

    cells can be added now

--- reactapp/src/components/Navbar.js
+++ reactapp/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = (props) => {
     <div>
     
   
-<nav className='navbar navbar-expand-lg ' style={{'backgroundColor': '#e3f2fd'}}>
+<nav className='navbar navbar-expand-lg ' style={{'backgroundColor': '#0E7680','color':'white'}}>
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,19 +14,19 @@ const Navbar = (props) => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <a className="nav-link inactive" aria-current="page" style={{'color':'white'}} href="/">Dimiour</a>
         </li>
                        
                         
                         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a className="nav-link dropdown-toggle" href="/" style={{'color':'white'}}  role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Services
           </a>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#hdc">hourly $ cost</a></li>
-            <li><a className="dropdown-item" href="/">Another service</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
+            <li><a className="dropdown-item" href="/">hourly $ cost</a></li>
+            <li><a className="dropdown-item" href="/">Overhead</a></li>
+            
+            <li><a className="dropdown-item" href="/">Sell Rate rounded</a></li>
           </ul>
         </li>
           
@@ -37,7 +37,7 @@ const Navbar = (props) => {
                   <input className="form-check-input"  type="checkbox"  id="flexSwitchCheckDefault" />
                   <label className="form-check-label" HTMLfor="flexSwitchCheckChecked">Enable DarkMode</label>
                   </div> */}
-                  <button type="button" class="btn btn-outline" style={{'color':'#0B1340','backgroundColor':'white'}} onClick={props.toggleMode}>Enable {props.mode==='light'?'dark':'light'} mode</button>
+                  <button type="button" className="btn btn-outline" style={{'color':'#0B1340','backgroundColor':'white'}} onClick={props.toggleMode}>Enable {props.mode==='light'?'dark':'light'} mode</button>
 
 
                 </div>
